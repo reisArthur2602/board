a1f3705e99cd771681cdd9d674b31817146c4207
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-15 20:47:18 -0300

    feat: add login com google

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,12 @@
+"use client"
 import React from "react"
 import { Container } from "./container"
 import Image from "next/image"
+import { signIn } from "next-auth/react"
 
 export const Header = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+
   return (
     <header className="bg-white py-7">
       <Container className="flex items-center justify-between">
@@ -12,7 +16,12 @@ export const Header = () => {
           width={120}
           height={44}
         />
-        <button className="bg-slate-800 text-slate-50">Comece Agora</button>
+        <button
+          className="bg-slate-800 text-slate-50"
+          onClick={handleLoginWithGoogleClick}
+        >
+          Comece Agora
+        </button>
       </Container>
     </header>
   )

de7fae17e2496017ec67342d4ab7540d8b526bc4
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-15 19:27:23 -0300

    feat: add Container component

---
+++ app/components/container.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { twMerge } from "tailwind-merge"
+
+type ContainerProps = React.HTMLAttributes<HTMLDivElement>
+
+export const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div className={twMerge("mx-auto w-full max-w-7xl", className)}>
+      {children}
+    </div>
+  )
+}

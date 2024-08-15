d46d21878a5f771512f63ed01772db558a6303ea
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-15 19:17:35 -0300

    style: estilos do layout global

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,29 @@
-import type { Metadata } from 'next';
-import { Nunito } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next"
+import { Nunito } from "next/font/google"
+import "./globals.css"
 
-const nunito = Nunito({ subsets: ['latin'] });
+const nunito = Nunito({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Dashboard - Mais que atender, entender',
-  description: 'Gerencie sua empresa, atendimentos e clientes',
+  title: "Dashboard - Mais que atender, entender",
+  description: "Gerencie sua empresa, atendimentos e clientes",
   icons: {
-    icon: '/board-favicon.svg',
+    icon: "/board-favicon.svg",
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <div className="flex h-full flex-col">
+          <div className="flex-1">{children}</div>
+        </div>
+      </body>
     </html>
-  );
+  )
 }

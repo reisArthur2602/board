2d061228b6f8f85e232f5b3b6b4a726bfb92b35b
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-16 14:50:06 -0300

    feat: add BackButton component

--- app/components/back-button.tsx
+++ app/components/back-button.tsx
@@ -1,3 +1,16 @@
-export const BackButton = () => {
-  return <button className="border border-solid">BackButton</button>;
+import { ChevronLeftIcon } from '@heroicons/react/16/solid';
+import Link from 'next/link';
+
+type BackButtonProps = {
+  href: string;
+};
+
+export const BackButton = ({ href }: BackButtonProps) => {
+  return (
+    <button className="w-fit border border-solid px-1">
+      <Link href={href}>
+        <ChevronLeftIcon className="size-6" />
+      </Link>
+    </button>
+  );
 };

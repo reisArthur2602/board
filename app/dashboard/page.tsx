4d9ed3f3b06f374ec12ffe5bfb78848f315d496b
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-16 12:28:07 -0300

    feat: add title and link to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container } from '../components/container';
+import Link from 'next/link';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <section className="mt-14 flex flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <h1>Chamados</h1>
+          <button className="bg-cyan-500 text-slate-50">
+            <Link href="/dashboard/new">Cadastrar Chamado</Link>
+          </button>
+        </div>
+      </section>
+    </Container>
+  );
 };
 
 export default Dashboard;

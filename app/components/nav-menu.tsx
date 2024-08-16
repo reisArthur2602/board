876ff3efde79ae82eafe91399a2c04a37262f1be
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-16 12:11:52 -0300

    feat: add NavMenu component

---
+++ app/components/nav-menu.tsx
@@ -0,0 +1,33 @@
+import { ChevronRightIcon } from '@heroicons/react/16/solid';
+import Link from 'next/link';
+
+type NavMenuProps = {
+  paths: {
+    href: string;
+    label: string;
+  }[];
+};
+
+export const NavMenu = ({ paths }: NavMenuProps) => {
+  return (
+    <nav>
+      <ol className="flex gap-3 space-x-2">
+        {paths.map((path, index) => (
+          <li key={index} className="flex items-center gap-3">
+            {index < paths.length - 1 ? (
+              <>
+                <Link href={path.href} className="hover:text-cyan-500">
+                  {path.label}
+                </Link>
+
+                <ChevronRightIcon className="size-5" />
+              </>
+            ) : (
+              <span className="font-bold text-cyan-500">{path.label}</span>
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+};

f74391bef70637dfe8edb98a81428ee7ce65b99d
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-16 17:28:15 -0300

    feat: add react-hook-forms and zod to FormCustomer component

--- app/dashboard/customer/new/component/form-customer.tsx
+++ app/dashboard/customer/new/component/form-customer.tsx
@@ -1,18 +1,98 @@
 'use client';
+
+import React, { useEffect } from 'react';
 import { BackButton } from '@/app/components/back-button';
 import { Input } from '@/app/components/input';
-import React from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { normalizeCNPJ, normalizePhoneNumber } from '@/app/utils/masks';
+
+const FormCustomerSchema = z.object({
+  email: z
+    .string()
+    .min(1, 'O campo email é obrigatório')
+    .email('Insira um email em um formato válido')
+    .trim(),
+  name: z.string().min(1, 'O campo nome é obrigatório').trim(),
+  phone: z
+    .string()
+    .min(1, 'O campo telefone é obrigatório')
+    .refine(
+      (value) => /\([1-9]{2}\) 9[1-9]\d{3}-\d{4}/.test(value),
+      'O campo telefone deve estar no formato (xx) xxxxx-xxxx.',
+    ),
+  cnpj: z
+    .string()
+    .min(1, 'O campo CNPJ é obrigatório')
+    .refine(
+      (value) => /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/.test(value),
+      'O campo CNPJ deve estar no formato xx.xxx.xxx/xxxx-xx',
+    ),
+});
 
 export const FormCustomer = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+    reset,
+  } = useForm<z.infer<typeof FormCustomerSchema>>({
+    resolver: zodResolver(FormCustomerSchema),
+  });
+
+  const phoneValue = watch('phone');
+  const cnpjValue = watch('cnpj');
+
+  useEffect(() => {
+    setValue('phone', normalizePhoneNumber(phoneValue));
+  }, [phoneValue, setValue]);
+
+  useEffect(() => {
+    setValue('cnpj', normalizeCNPJ(cnpjValue));
+  }, [cnpjValue, setValue]);
+
+  const handleCreateCustomer = async (
+    data: z.infer<typeof FormCustomerSchema>,
+  ) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <form className="flex flex-col gap-6">
+    <form
+      className="flex flex-col gap-6"
+      onSubmit={handleSubmit(handleCreateCustomer)}
+    >
       <div className="flex items-center gap-3">
-        <Input label="Email" placeholder="email@email.com" />
-        <Input label="Nome" placeholder="Ex: Arthur Reis" />
+        <Input
+          label="Email"
+          placeholder="email@email.com"
+          helperText={errors.email?.message}
+          {...register('email')}
+        />
+        <Input
+          label="Nome"
+          placeholder="Ex: Arthur Reis"
+          helperText={errors.name?.message}
+          {...register('name')}
+        />
       </div>
       <div className="flex items-center gap-3">
-        <Input label="Telefone" placeholder="(xx) xxxxx-xxxx" />
-        <Input label="CNPJ" placeholder="xx.xxx.xxx/xxxx-xx" />
+        <Input
+          label="Telefone"
+          placeholder="(xx) xxxxx-xxxx"
+          {...register('phone')}
+          helperText={errors.phone?.message}
+        />
+        <Input
+          label="CNPJ"
+          placeholder="xx.xxx.xxx/xxxx-xx"
+          helperText={errors.cnpj?.message}
+          {...register('cnpj')}
+        />
       </div>
       <div className="flex items-center justify-between">
         <BackButton href="/dashboard/customer" />
---
+++ app/utils/masks.ts
@@ -0,0 +1,21 @@
+export const normalizePhoneNumber = (value: string | undefined) => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})(\d+?)/, '$1');
+};
+
+export const normalizeCNPJ = (value: string | undefined): string => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/^(\d{2})(\d)/, '$1.$2')
+    .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+    .replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3/$4')
+    .replace(/^(\d{2})\.(\d{3})\.(\d{3})\/(\d{4})(\d)/, '$1.$2.$3/$4-$5')
+    .slice(0, 18);
+};

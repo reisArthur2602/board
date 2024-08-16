28e39fc6d8203e7cb72d07aa1e136048c44a9ad4
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-16 11:38:05 -0300

    feat: add PrivateLayout dashboard routes

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import { getServerSession } from 'next-auth';
+import React, { ReactNode } from 'react';
+import { authOptions } from '../lib/auth';
+import { redirect } from 'next/navigation';
+
+type PrivateLayoutProps = {
+  children: ReactNode;
+};
+const PrivateLayout = async ({ children }: PrivateLayoutProps) => {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect('/');
+  return <>{children}</>;
+};
+
+export default PrivateLayout;

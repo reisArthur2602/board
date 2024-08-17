98fb4d6de2d57d818d6c7417b6a622c467442e68
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-17 07:34:01 -0300

    feat: add Select and Textarea to Input component

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -2,6 +2,9 @@ import React, { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface FieldProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 interface InputProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
   helperText?: string;
@@ -15,7 +18,7 @@ const Input = ({ children, helperText, label, ...props }: InputProps) => {
     <label
       {...props}
       className={twMerge(
-        'flex w-full flex-col gap-3 font-bold text-slate-800 outline-none',
+        'flex w-full flex-col gap-3 font-bold text-slate-800 *:outline-none',
         helperText && errorStyle,
       )}
     >
@@ -42,5 +45,37 @@ const Field = forwardRef<HTMLInputElement, FieldProps>(
 
 Field.displayName = 'Field';
 Input.Field = Field;
+
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ children, ...props }: SelectProps, ref) => {
+    return (
+      <select
+        className="font-normal placeholder:text-slate-600"
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </select>
+    );
+  },
+);
+
+Select.displayName = 'Select';
+Input.Select = Select;
+
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ ...props }: TextareaProps, ref) => {
+    return (
+      <textarea
+        className="h-[7.5rem] resize-none font-normal placeholder:text-slate-600"
+        {...props}
+        ref={ref}
+      />
+    );
+  },
+);
+
+Textarea.displayName = 'Textarea';
+Input.Textarea = Textarea;
 
 export { Input };

ea8857aa0c21fd0611631914590002815457bae3
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-17 12:47:52 -0300

    feat: add to navMenu to DetailsCustomer page

--- app/dashboard/customer/[id]/page.tsx
+++ app/dashboard/customer/[id]/page.tsx
@@ -1,13 +1,26 @@
 import { Container } from '@/app/components/container';
+import { NavMenu } from '@/app/components/nav-menu';
 import React from 'react';
 
 type DetailsCustomerProps = {
-  id: string;
+  params: { id: string };
 };
 
-const DetailsCustomer = ({ id }: DetailsCustomerProps) => {
-  console.log(id);
-  return <Container>DetailsCustomer</Container>;
+const DetailsCustomer = ({ params }: DetailsCustomerProps) => {
+  const paths = [
+    { href: '/dashboard', label: 'Dashboard' },
+    { href: '/dashboard/customer', label: 'Clientes' },
+    { href: '/dashboard/customer/details', label: 'Detalhes do cliente' },
+  ];
+  console.log(params.id);
+  return (
+    <Container>
+      <div className="my-14">
+        <NavMenu paths={paths} />
+      </div>
+      ...
+    </Container>
+  );
 };
 
 export default DetailsCustomer;

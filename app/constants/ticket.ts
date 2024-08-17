18420ebe1b1c1ba2a9f9fb66438686389076ce99
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-17 08:40:37 -0300

    feat: add react-hook-form and zod to FormTicket

--- app/constants/ticket.ts
+++ app/constants/ticket.ts
@@ -1,8 +1,8 @@
 export const CATEGORY_TICKET = [
-  { label: 'Hardware', value: 'HARDWARE ' },
-  { label: 'Software', value: 'SOFTWARE ' },
-  { label: 'Rede', value: 'NETWORK ' },
-  { label: 'Suporte Geral', value: 'GENERAL_SUPPORT ' },
+  { label: 'Hardware', value: 'HARDWARE' },
+  { label: 'Software', value: 'SOFTWARE' },
+  { label: 'Rede', value: 'NETWORK' },
+  { label: 'Suporte Geral', value: 'GENERAL_SUPPORT' },
 ];
 export const PRIORITY = [
   { label: 'Baixa', value: 'LOW' },
--- app/dashboard/new/components/form-ticket.tsx
+++ app/dashboard/new/components/form-ticket.tsx
@@ -2,21 +2,64 @@
 import { BackButton } from '@/app/components/back-button';
 import { Input } from '@/app/components/input';
 import { CATEGORY_TICKET, PRIORITY, TYPE_TICKET } from '@/app/constants/ticket';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Customer } from '@prisma/client';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 type FormTicketProps = {
   // userId: string;
   customers: Customer[];
 };
 
+const FormTicketSchema = z.object({
+  title: z.string().min(1, 'O campo Título é obrigatório'),
+
+  description: z.string().min(1, 'O campo Descrição é obrigatório'),
+
+  customerId: z.string().min(1, 'O campo Cliente é obrigatório'),
+
+  type: z.enum(['INCIDENT', 'REQUEST', 'PROBLEM', 'CHANGE'], {
+    message:
+      'O campo Tipo deve ser um dos seguintes: Incidente, Solicitação, Problema, Mudança',
+  }),
+
+  category: z.enum(['HARDWARE', 'SOFTWARE', 'NETWORK', 'GENERAL_SUPPORT'], {
+    message:
+      'O campo Categoria deve ser um dos seguintes: Hardware, Software, Rede, Suporte Geral',
+  }),
+
+  priority: z.enum(['LOW', 'AVERAGE', 'HIGH', 'CRITICAL'], {
+    message:
+      'O campo Prioridade deve ser um dos seguintes: Baixa, Média, Alta, Crítica',
+  }),
+});
+
 export const FormTicket = ({ customers }: FormTicketProps) => {
   const isDisableInput = customers.length === 0;
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<z.infer<typeof FormTicketSchema>>({
+    resolver: zodResolver(FormTicketSchema),
+  });
+
+  const handleCreateTicket = async (data: z.infer<typeof FormTicketSchema>) => {
+    console.log(data);
+    reset();
+  };
+  console.log(errors);
   return (
-    <form className="flex flex-col gap-6">
-      <Input label="Cliente">
-        <Input.Select disabled={isDisableInput}>
+    <form
+      className="flex flex-col gap-6"
+      onSubmit={handleSubmit(handleCreateTicket)}
+    >
+      <Input label="Cliente" helperText={errors.customerId?.message}>
+        <Input.Select disabled={isDisableInput} {...register('customerId')}>
           <option value="">Selecione um Cliente</option>
           {customers &&
             customers.map((customer) => (
@@ -28,8 +71,8 @@ export const FormTicket = ({ customers }: FormTicketProps) => {
       </Input>
 
       <div className="flex items-center gap-3">
-        <Input label="Tipo">
-          <Input.Select disabled={isDisableInput}>
+        <Input label="Tipo" helperText={errors.type?.message}>
+          <Input.Select disabled={isDisableInput} {...register('type')}>
             <option value="">Selecione um Tipo</option>
             {TYPE_TICKET.map((type) => (
               <option value={type.value} key={type.value}>
@@ -39,8 +82,8 @@ export const FormTicket = ({ customers }: FormTicketProps) => {
           </Input.Select>
         </Input>
 
-        <Input label="Categoria">
-          <Input.Select disabled={isDisableInput}>
+        <Input label="Categoria" helperText={errors.category?.message}>
+          <Input.Select disabled={isDisableInput} {...register('category')}>
             <option value="">Selecione uma Categoria</option>
             {CATEGORY_TICKET.map((category) => (
               <option value={category.value} key={category.value}>
@@ -50,8 +93,8 @@ export const FormTicket = ({ customers }: FormTicketProps) => {
           </Input.Select>
         </Input>
 
-        <Input label="Prioridade">
-          <Input.Select disabled={isDisableInput}>
+        <Input label="Prioridade" helperText={errors.priority?.message}>
+          <Input.Select disabled={isDisableInput} {...register('priority')}>
             <option value="">Selecione uma Prioridade</option>
             {PRIORITY.map((priority) => (
               <option value={priority.value} key={priority.value}>
@@ -62,16 +105,18 @@ export const FormTicket = ({ customers }: FormTicketProps) => {
         </Input>
       </div>
 
-      <Input label="Título">
+      <Input label="Título" helperText={errors.title?.message}>
         <Input.Field
           placeholder="Título do chamado..."
           disabled={isDisableInput}
+          {...register('title')}
         />
       </Input>
-      <Input label="Descrição">
+      <Input label="Descrição" helperText={errors.description?.message}>
         <Input.Textarea
           placeholder="Descreva o seu problema..."
           disabled={isDisableInput}
+          {...register('description')}
         />
       </Input>
 

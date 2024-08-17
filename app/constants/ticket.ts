1db3d0d2c6a76e3a9270fc695acdfb63ba9c40f1
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-17 07:54:57 -0300

    feat: add constants as options values to FormTicket

---
+++ app/constants/ticket.ts
@@ -0,0 +1,18 @@
+export const CATEGORY_TICKET = [
+  { label: 'Hardware', value: 'HARDWARE ' },
+  { label: 'Software', value: 'SOFTWARE ' },
+  { label: 'Rede', value: 'NETWORK ' },
+  { label: 'Suporte Geral', value: 'GENERAL_SUPPORT ' },
+];
+export const PRIORITY = [
+  { label: 'Baixa', value: 'LOW' },
+  { label: 'Média', value: 'AVERAGE' },
+  { label: 'Alta', value: 'HIGH' },
+  { label: 'Crítica', value: 'CRITICAL' },
+];
+export const TYPE_TICKET = [
+  { label: 'INCIDENT', value: 'INCIDENT' },
+  { label: 'Solicitação', value: 'REQUEST' },
+  { label: 'Problema', value: 'PROBLEM' },
+  { label: 'Mudança', value: 'CHANGE' },
+];
--- app/dashboard/new/components/form-ticket.tsx
+++ app/dashboard/new/components/form-ticket.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { BackButton } from '@/app/components/back-button';
 import { Input } from '@/app/components/input';
+import { CATEGORY_TICKET, PRIORITY, TYPE_TICKET } from '@/app/constants/ticket';
 import { Customer } from '@prisma/client';
 import React from 'react';
 
@@ -30,18 +31,33 @@ export const FormTicket = ({ customers }: FormTicketProps) => {
         <Input label="Tipo">
           <Input.Select disabled={isDisableInput}>
             <option value="">Selecione um Tipo</option>
+            {TYPE_TICKET.map((type) => (
+              <option value={type.value} key={type.value}>
+                {type.label}
+              </option>
+            ))}
           </Input.Select>
         </Input>
 
         <Input label="Categoria">
           <Input.Select disabled={isDisableInput}>
             <option value="">Selecione uma Categoria</option>
+            {CATEGORY_TICKET.map((category) => (
+              <option value={category.value} key={category.value}>
+                {category.label}
+              </option>
+            ))}
           </Input.Select>
         </Input>
 
         <Input label="Prioridade">
           <Input.Select disabled={isDisableInput}>
             <option value="">Selecione uma Prioridade</option>
+            {PRIORITY.map((priority) => (
+              <option value={priority.value} key={priority.value}>
+                {priority.label}
+              </option>
+            ))}
           </Input.Select>
         </Input>
       </div>

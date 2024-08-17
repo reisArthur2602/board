d5e2fbae0a960ccc42f8be9fe852efd5c81d54b4
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-17 07:19:57 -0300

    feat: add new Ticket page

---
+++ app/dashboard/new/page.tsx
@@ -0,0 +1,8 @@
+import { Container } from '@/app/components/container';
+import React from 'react';
+
+const NewTicket = () => {
+  return <Container>NewTicket</Container>;
+};
+
+export default NewTicket;

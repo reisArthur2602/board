f6b1af31ba57918403945a5e5e381911c71d2c02
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-18 13:20:00 -0300

    feat: render customer to SearchResults page

--- app/dashboard/customer/search/[title]/page.tsx
+++ app/dashboard/customer/search/[title]/page.tsx
@@ -1,5 +1,9 @@
 import { Container } from '@/app/components/container';
+
 import { db } from '@/app/lib/prisma';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 
 type SearchResultsProps = {
   params: { title: string };
@@ -9,8 +13,60 @@ const SearchResults = async ({ params }: SearchResultsProps) => {
     where: { name: { contains: params.title, mode: 'insensitive' } },
   });
 
-  console.log(customers);
-  return <Container>SearchResults</Container>;
+  const TABLE_HEAD = [
+    { label: 'Nome' },
+    { label: 'Telefone' },
+    { label: 'CNPJ' },
+    { label: 'Email' },
+    { label: 'Cadastro' },
+  ];
+
+  return (
+    <Container className="py-14">
+      <h1 className="mb-6">Resultados para &quot;{params.title}&quot;</h1>
+      <table className="w-full min-w-[56.25rem] text-sm *:w-full *:text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((h, index) => (
+              <th key={index} className="px-3 py-4 font-bold text-slate-800">
+                {h.label}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {customers.map((c) => (
+            <tr
+              className="border-t border-solid *:px-3 *:py-4 hover:bg-slate-950/5"
+              key={c.id}
+            >
+              <td className="capitalize">
+                <Link href={`/dashboard/customer/${c.id}`}>{c.name}</Link>
+              </td>
+              <td>
+                <Link href={`/dashboard/customer/${c.id}`}>{c.phone}</Link>
+              </td>
+              <td>
+                <Link href={`/dashboard/customer/${c.id}`}>{c.cnpj}</Link>
+              </td>
+              <td>
+                <Link href={`/dashboard/customer/${c.id}`}>{c.email}</Link>
+              </td>
+              <td className="capitalize">
+                <Link href={`/dashboard/customer/${c.id}`}>
+                  {c.created_at &&
+                    format(c.created_at, 'MMMM dd, yyyy', {
+                      locale: ptBR,
+                    })}
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
 };
 
 export default SearchResults;

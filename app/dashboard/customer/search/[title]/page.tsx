2569dbc712437131a0f7174705951dba5d098837
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-18 13:13:47 -0300

    feat: search customer

--- app/dashboard/customer/search/[title]/page.tsx
+++ app/dashboard/customer/search/[title]/page.tsx
@@ -1,10 +1,15 @@
 import { Container } from '@/app/components/container';
+import { db } from '@/app/lib/prisma';
 
 type SearchResultsProps = {
   params: { title: string };
 };
-const SearchResults = ({ params }: SearchResultsProps) => {
-  console.log(params.title);
+const SearchResults = async ({ params }: SearchResultsProps) => {
+  const customers = await db.customer.findMany({
+    where: { name: { contains: params.title, mode: 'insensitive' } },
+  });
+
+  console.log(customers);
   return <Container>SearchResults</Container>;
 };
 
